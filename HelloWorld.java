829e511a6dab87dc134a44423f5fc494102296df
swathikaki <59576472+swathikaki@users.noreply.github.com>
2020-04-25 15:57:18 -0400

    Verifying branch concept

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld{
 
 public void main(String[] args){
    //Hello World
-  //2nd commit
-    System.out.println("Hellow To Java World"); 
+  
+    System.out.println("Hello to Java World"); 
   }
 
 }
